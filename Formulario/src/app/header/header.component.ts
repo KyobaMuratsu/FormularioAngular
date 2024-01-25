5d7fa731ee6ac9d4d33dff035956bbfdd07222b6
Bruno Bernado Tremarin <bruno-tremarin@hotmail.com>
2024-01-25 11:39:39 -0300

    Alterações para endereço

---
+++ Formulario/src/app/header/header.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent implements OnInit {
+
+  isPhonePortrait = false;
+
+  constructor(private responsive: BreakpointObserver){
+
+  }
+
+  ngOnInit() {
+      this.responsive.observe([Breakpoints.HandsetPortrait, Breakpoints.TabletPortrait]).subscribe(result => {
+        
+        this.isPhonePortrait = false;
+        const breakpoints = result.breakpoints
+
+        if (breakpoints[Breakpoints.TabletPortrait]){
+          console.log("screens matches")
+        }
+
+        if (breakpoints[Breakpoints.HandsetPortrait]) {
+          console.log("true");
+          this.isPhonePortrait = true
+        }
+
+      })
+  }
+
+}
--- Formulario/src/app/pagina-cadastro/pagina-cadastro.component.ts
+++ Formulario/src/app/pagina-cadastro/pagina-cadastro.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
 import {ErrorStateMatcher, provideNativeDateAdapter} from '@angular/material/core';
 import {
   FormControl,
@@ -12,19 +13,41 @@ import {
   FormsModule,
   ReactiveFormsModule,
 } from '@angular/forms';
+import { HeaderComponent } from '../header/header.component';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-pagina-cadastro',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule, FormsModule, ReactiveFormsModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, HeaderComponent, MatIconModule],
   templateUrl: './pagina-cadastro.component.html',
   styleUrl: './pagina-cadastro.component.css'
 })
-export class PaginaCadastroComponent {
+export class PaginaCadastroComponent implements OnInit {
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
   matcher = new MyErrorStateMatcher();
+
+  constructor(private responsive: BreakpointObserver){
+
+  }
+
+  ngOnInit() {
+      this.responsive.observe([Breakpoints.HandsetLandscape, Breakpoints.TabletPortrait]).subscribe(result => {
+
+        const breakpoints = result.breakpoints
+
+        if (breakpoints[Breakpoints.TabletPortrait]){
+          console.log("screens matches")
+        }
+
+        if (breakpoints[Breakpoints.HandsetLandscape]) {
+
+        }
+
+      })
+  }
 
 }
 
