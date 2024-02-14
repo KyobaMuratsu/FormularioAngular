60e77600f5ba19a77f6d93d6cc0a836bd36ae552
Bruno Bernado Tremarin <bruno-tremarin@hotmail.com>
2024-02-14 12:17:37 -0300

    Problemas com Hydration

--- Formulario/src/app/header/header.component.ts
+++ Formulario/src/app/header/header.component.ts
@@ -4,17 +4,22 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, CommonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit {
 
+  isDesktop = false
   isPhonePortrait = false;
+  isPhoneLandscape = false;
+  isTabletPortrait = false;
+  isTabletLandscape = false;
 
   @Input() titleT = '';
 
@@ -23,19 +28,34 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit() {
-      this.responsive.observe([Breakpoints.HandsetPortrait, Breakpoints.TabletPortrait]).subscribe(result => {
+      this.responsive.observe([Breakpoints.Handset, Breakpoints.Tablet, Breakpoints.HandsetLandscape, Breakpoints.HandsetLandscape]).subscribe(result => {
         
         this.isPhonePortrait = false;
+        this.isPhoneLandscape = false;
+        this.isTabletPortrait = false;
+        this.isTabletLandscape = false;
+
         const breakpoints = result.breakpoints
 
         if (breakpoints[Breakpoints.TabletPortrait]){
-          console.log("screens matches")
+          console.log("screens matches Tablet Portrait")
+          this.isTabletPortrait = true
+        }
+
+        if (breakpoints[Breakpoints.TabletLandscape]){
+          console.log("screens match Tablet")
+          this.isTabletLandscape = true
         }
 
         if (breakpoints[Breakpoints.HandsetPortrait]) {
-          console.log("true");
+          console.log("screens match PHONE");
           this.isPhonePortrait = true
         }
+
+        if(this.isTabletPortrait === false){
+            this.isDesktop = true;
+        }else
+            this.isDesktop = false;
 
       })
   }
